3b53aec5bb5bf13d7dc58d3c3f844de7acb83685
gokmen.yilmaz <gokmen1977@gmail.com>
2020-05-29 19:40:47 +0300

    hhh

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { fetchRepos } from "./api.js";
 import Loading from "./Loading";
-import ReposGrid from "./reposGrid"
+import ReposGridHook from "./reposGrid"
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<ReposGrid />, rootElement);
+ReactDOM.render(<ReposGridHook />, rootElement);
